d30df28ceb4b18b80965f81c84bd28d4fd02fc27
Magnus <magnus.rinnan@gmail.com>
2024-07-17 07:04:13 +0000

    m

--- .github/clone_repos.sh
+++ .github/clone_repos.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+sudo apt install ssh-askpass
+ssh-keyscan -t rsa github.com >>~/.ssh/known_hosts
 eval $(ssh-agent)
 ssh-add - <<<"${PRIVATE_SSH_KEY}"
-pip install -r requirements.txt
+# echo "$PRIVATE_SSH_KEY" | tr -d '\r' | ssh-add - >/dev/null
+echo "Cloning repos..."
+#clone subrepo
+git clone "git@github.com:magnusriga/nfront.git"
+# pip install -r requirements.txt
